ab7e97fa31ee81b470a2e5f8b03696ac94da63ae
HuangChILun <60692423+HuangChILun@users.noreply.github.com>
2024-08-04 23:11:31 -0600

    sign out function

--- src/app/components/SideMenu.js
+++ src/app/components/SideMenu.js
@@ -1,12 +1,22 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useUserAuth } from "@/app/_utils/auth-context";
 
 const Sidebar = () => {
-    const [activeTab, setActiveTab] = useState('view-patient');
-    const handleTabClick = (tab) => {
-        setActiveTab(tab);
-      };
+  const { firebaseSignOut } = useUserAuth();
+  const [activeTab, setActiveTab] = useState('view-patient');
+  const router = useRouter();
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const handleSignOut = async () => {
+    await firebaseSignOut();
+    router.push('/'); // Redirect to the homepage after signing out
+  };
 
   return (
     <div className="sidebar bg-gray-100 h-screen p-5">
@@ -15,17 +25,20 @@ const Sidebar = () => {
       </div>
       <nav className="mb-5">
         <ul>
-        <li className={`mb-3 ${activeTab === 'view-patient' ? 'active-link' : ''}`}>
-            <Link href="/pages/view-patient-list" className="flex items-center text-lg cursor-pointer"
-            onClick={() => handleTabClick('view-patient')}>
+          <li className={`mb-3 ${activeTab === 'view-patient' ? 'active-link' : ''}`}>
+            <Link href="/pages/view-patient-list" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('view-patient')}>
               <span className="ml-2">View Patient</span>
             </Link>
           </li>
           <li className={`mb-3 ${activeTab === 'add-new-patient' ? 'active-link' : ''}`}>
-            <Link href="/pages/add-new-patient" className="flex items-center text-lg cursor-pointer"
-            onClick={() => handleTabClick('add-new-patient')}>
+            <Link href="/pages/add-new-patient" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('add-new-patient')}>
               <span className="ml-2">Add New Patient</span>
             </Link>
+          </li>
+          <li className="mb-3">
+            <button onClick={handleSignOut} className="flex items-center text-lg cursor-pointer text-red-600">
+              <span className="ml-2">Sign Out</span>
+            </button>
           </li>
         </ul>
       </nav>
