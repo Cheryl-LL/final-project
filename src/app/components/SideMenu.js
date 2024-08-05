1f751ee3edb9c1175ad06b7b01c1a1111b9e25a8
HuangChILun <60692423+HuangChILun@users.noreply.github.com>
2024-08-04 23:15:18 -0600

    change style for sideMenu

--- src/app/components/SideMenu.js
+++ src/app/components/SideMenu.js
@@ -19,29 +19,31 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar bg-gray-100 h-screen p-5">
-      <div className="mb-5">
-        <h1 className="text-2xl font-bold">Bridging Abilities</h1>
+    <div className="sidebar bg-gray-100 h-screen flex flex-col justify-between p-5">
+      <div>
+        <div className="mb-5">
+          <h1 className="text-2xl font-bold">Bridging Abilities</h1>
+        </div>
+        <nav className="mb-5">
+          <ul>
+            <li className={`mb-3 ${activeTab === 'view-patient' ? 'active-link' : ''}`}>
+              <Link href="/pages/view-patient-list" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('view-patient')}>
+                <span className="ml-2">View Patient</span>
+              </Link>
+            </li>
+            <li className={`mb-3 ${activeTab === 'add-new-patient' ? 'active-link' : ''}`}>
+              <Link href="/pages/add-new-patient" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('add-new-patient')}>
+                <span className="ml-2">Add New Patient</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="mb-5">
-        <ul>
-          <li className={`mb-3 ${activeTab === 'view-patient' ? 'active-link' : ''}`}>
-            <Link href="/pages/view-patient-list" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('view-patient')}>
-              <span className="ml-2">View Patient</span>
-            </Link>
-          </li>
-          <li className={`mb-3 ${activeTab === 'add-new-patient' ? 'active-link' : ''}`}>
-            <Link href="/pages/add-new-patient" className="flex items-center text-lg cursor-pointer" onClick={() => handleTabClick('add-new-patient')}>
-              <span className="ml-2">Add New Patient</span>
-            </Link>
-          </li>
-          <li className="mb-3">
-            <button onClick={handleSignOut} className="flex items-center text-lg cursor-pointer text-red-600">
-              <span className="ml-2">Sign Out</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
+      <div className="mt-auto">
+        <button onClick={handleSignOut} className="flex items-center text-lg cursor-pointer text-red-600">
+          <span className="ml-2">Sign Out</span>
+        </button>
+      </div>
     </div>
   );
 };
